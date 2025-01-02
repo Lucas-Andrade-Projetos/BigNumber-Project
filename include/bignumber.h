9fa4f40c8711d1681187b29eec396bbccbd8b5db
Lucas-Andrade <lucas.andradeprojetos@gmail.com>
2025-01-02 12:09:48 -0300

    --git commit -dev [Lucas Andrade] Divisão feita. Versão de testes

--- include/bignumber.h
+++ include/bignumber.h
@@ -17,6 +17,7 @@ typedef struct {
 //----------------------------------------------------------------Funções de Controle e Teste
 BigNumber* criarNo(int digito); //Função para criar nó com um dígito
 BigNumberStruct* criarBigNumber(); //Função para inicializar um BigNumber com sinal
+BigNumberStruct* criarBigNumberDeNumero(long long numero); //Função para criar um bignumber já com valor definido
 void adicionarNoFim(BigNumberStruct* numero, int digito); //Função para adicionar nó no fim da lista
 void adicionarInicio(BigNumberStruct* numero, int digito); //Função para adicionar nó no início da lista
 void adicionarAntes(BigNumberStruct* numero, int valor, int novoDigito);//Função para adicionar antes de um valor
@@ -25,20 +26,22 @@ void lerNumeroComSinal(BigNumberStruct* numero); //Função para ler um número 
 void removerNumero(BigNumberStruct* numero, int digito); //Função para remover um nó com o número especificado
 BigNumber* encontrarNumero(BigNumberStruct* numero, int digito); //Função para encontrar um nó na lista
 int contarElementos(BigNumberStruct* numero); //Função para contar o número de elementos na lista
-int comparaBigNumbers(BigNumberStruct* num1, BigNumberStruct* num2);
-void removerInicio(BigNumberStruct* numero);
-void removerZeros(BigNumberStruct* numero);
-BigNumberStruct* reverterBigNumber(BigNumberStruct* numero);
+int comparaBigNumbers(BigNumberStruct* num1, BigNumberStruct* num2); //Funçao que compara a magnetudo dos dígitos, ou seja vê qual é maior que o outro.
+void removerInicio(BigNumberStruct* numero); //Função para remover numeros no inicio
+void removerZeros(BigNumberStruct* numero); //Função que remove os zeros a esquerda.
+BigNumberStruct* reverterBigNumber(BigNumberStruct* numero); //Função que reverte a lista(Usada na multiplicação)
 void imprimirNumero(BigNumberStruct* numero); //Função para imprimir a lista encadeada
 void liberaMemoria(BigNumberStruct* numero); //Função para liberar a memória da lista
 
 void adicionarNoFinal(BigNumber** head, int digito);
 
 //----------------------------------------------------------------Funções de Operações
-BigNumberStruct* executarOperacao(char operacao, BigNumberStruct* num1, BigNumberStruct* num2);
-BigNumberStruct* somaBigNumber(BigNumberStruct* num1, BigNumberStruct* num2);
-BigNumberStruct* subtrairBigNumbers(BigNumberStruct* numero1, BigNumberStruct* numero2);
-BigNumberStruct* multiplicarBigNumbers(BigNumberStruct* num1, BigNumberStruct* num2);
+BigNumberStruct* executarOperacao(char operacao, BigNumberStruct* num1, BigNumberStruct* num2); //Função para controlar a operação escolhida
+BigNumberStruct* somaBigNumber(BigNumberStruct* num1, BigNumberStruct* num2); //Função para somar dois BigNumberStruct
+BigNumberStruct* subtrairBigNumbers(BigNumberStruct* numero1, BigNumberStruct* numero2); //Função para subtrair dois BigNumberStruct
+BigNumberStruct* multiplicarBigNumbers(BigNumberStruct* num1, BigNumberStruct* num2); //Função para multiplicar dois BigNumberStruct
+BigNumberStruct* dividirBigNumbers(BigNumberStruct* dividendo, BigNumberStruct* divisor); //Função para dividir dois BigNumberStruct com resultado inteiro.
+
 
 
 
